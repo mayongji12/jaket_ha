70a9262fed751968e50eba49907aa036b425d53f
mayongji12 <mayongji123@163.com>
2016-09-28 15:15:25 +0800

    Update database_master.sh

--- database_master.sh
+++ database_master.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-mount /dev/vgstore/lvu1 /u1
-mount /dev/vgstore/lvu2 /u2
+mount  /app
+mount /database
 oracledir=/u2/oracle/product/11.2.0/dbhome_1/network/admin
 cd $oracledir
-cp -rfp listener.ora.241 listener.ora;cp -rfp tnsnames.ora.241 tnsnames.ora
-ifconfig eth0:1 192.168.58.242 netmask 255.255.255.0 up
-ifconfig eth0:2 192.168.58.243 netmask 255.255.255.0 up
+cp -rfp listener.ora.master listener.ora;cp -rfp tnsnames.ora.master tnsnames.ora
+ifconfig eth0:1 virip1 netmask ns1 up
+ifconfig eth0:2 virip2 netmask ns2 up
 su - oracle -c "lsnrctl start;dbstart"
