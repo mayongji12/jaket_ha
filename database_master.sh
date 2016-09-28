702bcf3d56c29d28ce8d95ebfa5dd427eae9409a
mayongji12 <mayongji123@163.com>
2016-09-28 15:15:50 +0800

    Update database_master.sh

--- database_master.sh
+++ database_master.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mount  /app
 mount /database
-oracledir=/u2/oracle/product/11.2.0/dbhome_1/network/admin
+oracledir=$ORACLE_HOME
 cd $oracledir
 cp -rfp listener.ora.master listener.ora;cp -rfp tnsnames.ora.master tnsnames.ora
 ifconfig eth0:1 virip1 netmask ns1 up
